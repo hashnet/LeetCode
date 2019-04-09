20ec60dc1e92e80a32a85a3997904f588b56adeb
Sheikh Mohammad Maidul Hasan <Sheikh.Hasan@FINEOS.com>
2019-04-09 17:09:09 +1000

    1 Two Sum, 2 Add Two Numbers

---
+++ Problemset_All/src/com/hashnet/leetcode/problemsetall/_1_Two_Sum.java
@@ -0,0 +1,33 @@
+package com.hashnet.leetcode.problemsetall;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1_Two_Sum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int diff = target - nums[i];
+
+			Integer index = map.get(diff);
+			if (index != null) {
+				return new int[] { index, i };
+			}
+
+			map.put(nums[i], i);
+		}
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 5 };
+		int target = 9;
+
+		_1_Two_Sum solution = new _1_Two_Sum();
+		System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+	}
+}
