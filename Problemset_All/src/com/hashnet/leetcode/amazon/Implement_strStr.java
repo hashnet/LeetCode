4a2e68bc18e52ccc24006bf9a453592684d437ab
hashnet <maidul.hasan@gmail.com>
2019-06-14 09:14:56 +1000

    Add more projects

---
+++ Problemset_All/src/com/hashnet/leetcode/amazon/Implement_strStr.java
@@ -0,0 +1,32 @@
+package com.hashnet.leetcode.amazon;
+
+public class Implement_strStr {
+	public int strStr(String haystack, String needle) {
+		if(haystack == null || needle == null) throw new IllegalArgumentException();
+		
+		if(needle.equals("")) return 0;
+		
+		for(int i=0; i<=(haystack.length() - needle.length()); i++) {
+			boolean found = true;
+			for(int j=0; j<needle.length(); j++) {
+				if(haystack.charAt(i+j) != needle.charAt(j)) {
+					found = false;
+					break;
+				}
+			}
+			
+			if(found) return i;
+		}
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Implement_strStr solution = new Implement_strStr();
+		
+		System.out.println(solution.strStr("hello", "ll"));
+		System.out.println(solution.strStr("mississippi", "mississippi"));
+		System.out.println(solution.strStr("aaaa", "ab"));
+		System.out.println(solution.strStr("a", "a"));
+	}
+}
---
+++ Problemset_All/src/com/hashnet/leetcode/amazon/Missing_Number.java
@@ -0,0 +1,23 @@
+package com.hashnet.leetcode.amazon;
+
+import java.util.Arrays;
+
+import com.hashnet.leetcode.utility.ArrayUtility;
+
+public class Missing_Number {
+	public int missingNumber(int[] nums) {
+		int len = nums.length;
+		int tot = (len * (len+1)) / 2;
+		
+		int sum = 0;
+		for(int i=0; i<len; i++) sum += nums[i];
+		
+		return tot - sum;
+	}
+
+	public static void main(String[] args) {
+		Missing_Number solution = new Missing_Number();
+		
+		System.out.println(solution.missingNumber(ArrayUtility.toIntArray(9,6,4,2,3,5,7,0,1)));
+	}
+}
