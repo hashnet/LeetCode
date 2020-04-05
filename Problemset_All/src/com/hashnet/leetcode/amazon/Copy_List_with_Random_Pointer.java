df3602c5c8e7854c81ebe88e49f1282f82a6af6d
Sheikh Mohammad Maidul Hasan <maidul.hasan@gmail.com>
2020-04-05 16:45:21 +1000

    Updated Copy List with Random Pointer

--- Problemset_All/src/com/hashnet/leetcode/amazon/Copy_List_with_Random_Pointer.java
+++ Problemset_All/src/com/hashnet/leetcode/amazon/Copy_List_with_Random_Pointer.java
@@ -7,58 +7,46 @@ import java.util.Map;
 
 public class Copy_List_with_Random_Pointer {
 	private class Node {
-	    public int val;
-	    public Node next;
-	    public Node random;
+		int val;
+		Node next;
+		Node random;
 
-	    public Node() {}
-
-	    public Node(int _val,Node _next,Node _random) {
-	        val = _val;
-	        next = _next;
-	        random = _random;
-	    }
+		public Node(int val) {
+			this.val = val;
+			this.next = null;
+			this.random = null;
+		}
 	}
-	
+
     public Node copyRandomList(Node head) {
     	if(head == null) return null;
-    	
-    	Map<Node, Integer> table = new HashMap<>();
+
+    	Map<Node, Node> map = new HashMap<>();
+		int idx = 0;
+		Node originalIterator = head;
+		Node newHead = new Node(0);
+		Node prevNode = newHead;
+		while(originalIterator != null) {
+    		Node node = new Node(originalIterator.val);
+    		map.put(originalIterator, node);
+    		prevNode.next = node;
+
+    		prevNode = node;
+    		originalIterator = originalIterator.next;
+		}
+		newHead = newHead.next;
+
+		originalIterator = head;
+		Node currIterator = newHead;
+		while(originalIterator != null) {
+			currIterator.random = originalIterator.random == null ? null : map.get(originalIterator.random);
+
+			originalIterator = originalIterator.next;
+			currIterator = currIterator.next;
+		}
+
         
-        List<Node> newNodeList = new ArrayList<>();
-        Node curr = head;
-        int index = 0;
-        while(curr != null) {
-        	table.put(curr, index);
-        	
-        	Node newNode = new Node();
-        	newNode.val = curr.val;
-        	newNodeList.add(newNode);
-        	
-        	curr = curr.next;
-        	++index;
-        }
-        
-        for(int i=0; i<newNodeList.size()-1; i++) {
-        	newNodeList.get(0).next = newNodeList.get(i+1);
-        }
-        newNodeList.get(newNodeList.size()-1).next = null;
-        
-        curr = head;
-        index = 0;
-        while(curr != null) {
-        	if(curr.random == null) {
-        		newNodeList.get(index).random = null;
-        	} else {
-	        	int randIndex = table.get(curr.random);
-	        	newNodeList.get(index).random = newNodeList.get(randIndex);
-        	}
-        	
-        	curr = curr.next;
-        	++index;
-        }
-        
-        return newNodeList.get(0);
+        return newHead;
     }
     
     public static void main(String[] args) {
--- Problemset_All/src/com/hashnet/leetcode/amazon/Reverse_Linked_List.java
+++ Problemset_All/src/com/hashnet/leetcode/amazon/Reverse_Linked_List.java
@@ -7,14 +7,15 @@ public class Reverse_Linked_List {
 	public ListNode reverseList(ListNode head) {
 		ListNode prev = null;
 		ListNode curr = head;
+
 		while(curr != null) {
 			ListNode next = curr.next;
 			curr.next = prev;
-			
+
 			prev = curr;
 			curr = next;
 		}
-		
+
 		return prev;
 	}
 
