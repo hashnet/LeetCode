920a273668efcf260f08c96b93a8f6fd87a38361
Sheikh Mohammad Maidul Hasan <maidul.hasan@gmail.com>
2020-05-22 13:54:28 +1000

    Added new solutions

---
+++ Problemset_All/src/com/hashnet/leetcode/amazon/Smallest_Integer_Not_Occuring_in_A.java
@@ -0,0 +1,27 @@
+package com.hashnet.leetcode.amazon;
+
+import java.util.Arrays;
+
+public class Smallest_Integer_Not_Occuring_in_A {
+    public int solution(int[] A) {
+        int max = 1;
+
+        Arrays.sort(A);
+
+        for(int i : A) {
+            if(i == max) ++max;
+            else if(i < max) continue;
+            else return max;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Smallest_Integer_Not_Occuring_in_A s = new Smallest_Integer_Not_Occuring_in_A();
+        System.out.println(s.solution(new int[]{1, 3, 6, 4, 1, 2}));
+        System.out.println(s.solution(new int[]{1, 2, 3}));
+        System.out.println(s.solution(new int[]{-1, -3}));
+        System.out.println(s.solution(new int[]{Integer.MIN_VALUE}));
+    }
+}
---
+++ Problemset_All/src/com/hashnet/leetcode/problemsetall/_525_Contiguous_Array.java
@@ -0,0 +1,32 @@
+package com.hashnet.leetcode.problemsetall;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _525_Contiguous_Array {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int max = 0;
+        int count = 0;
+        map.put(0, -1);
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0) --count;
+            else ++count;
+
+            if(map.containsKey(count)) {
+                max = Math.max(max, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        _525_Contiguous_Array solution = new _525_Contiguous_Array();
+
+        System.out.println(solution.findMaxLength(new int[] {0,0,1,0,0,0,1,1}));
+    }
+}
