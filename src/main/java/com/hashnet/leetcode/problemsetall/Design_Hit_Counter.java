49896238e12dacee947e83d208979eb3f8a64b9e
sheikh.hasan <sheikh.hasan@adevinta.com>
2022-08-17 20:44:14 +1000

    Added New Solutions

---
+++ src/main/java/com/hashnet/leetcode/problemsetall/Design_Hit_Counter.java
@@ -0,0 +1,42 @@
+package com.hashnet.leetcode.problemsetall;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Design_Hit_Counter {
+    private static class HitCounter {
+        private Queue<Integer> queue;
+
+        public HitCounter() {
+            queue = new ArrayDeque<>();
+        }
+
+        public void hit(int timestamp) {
+            while(queue.peek() != null && queue.peek() <= timestamp-300) {
+                queue.remove();
+            }
+
+            queue.add(timestamp);
+        }
+
+        public int getHits(int timestamp) {
+            while(queue.peek() != null && queue.peek() <= timestamp-300) {
+                queue.remove();
+            }
+
+            return queue.size();
+        }
+    }
+
+    public static void main(String[] args) {
+        Design_Hit_Counter.HitCounter solution = new Design_Hit_Counter.HitCounter();
+        solution.hit(1);
+        solution.hit(2);
+        solution.hit(3);
+        System.out.println(solution.getHits(4));
+        solution.hit(300);
+        System.out.println(solution.getHits(300));
+        System.out.println(solution.getHits(301));
+    }
+}
