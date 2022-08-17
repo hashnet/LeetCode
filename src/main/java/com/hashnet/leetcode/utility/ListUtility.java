679dc72a69262c9a0ac41e6e7789e3751a493316
sheikh.hasan <sheikh.hasan@adevinta.com>
2022-08-17 21:31:11 +1000

    Updated ListUtility

--- src/main/java/com/hashnet/leetcode/utility/ListUtility.java
+++ src/main/java/com/hashnet/leetcode/utility/ListUtility.java
@@ -2,14 +2,27 @@ package com.hashnet.leetcode.utility;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListUtility {
+
     public static String prettyPrint(Object objects) {
-        if (objects instanceof Collection) {
+        if (objects instanceof Set) {
+            return ((Set<Object>) objects).stream()
+                .map(ListUtility::prettyPrint)
+                .collect(Collectors.joining(",", "(", ")"));
+        } else if (objects instanceof Collection) {
             return (String) ((Collection) objects).stream()
-                    .map(ListUtility::prettyPrint)
-                    .collect(Collectors.joining(",", "[", "]"));
+                .map(ListUtility::prettyPrint)
+                .collect(Collectors.joining(",", "[", "]"));
+        } else if (objects instanceof Map) {
+            return ((Map<Object, Object>) objects).entrySet()
+                .stream()
+                .map(e -> prettyPrint(e.getKey()) + ":" + prettyPrint(e.getValue()))
+                .collect(Collectors.joining(",", "{", "}"));
         } else {
             return String.valueOf(objects);
         }
@@ -23,5 +36,7 @@ public class ListUtility {
         System.out.println(prettyPrint(List.of("A", 23, "D")));
         System.out.println(prettyPrint(List.of("A", List.of(2, 3), "D")));
         System.out.println(prettyPrint(List.of("A", List.of(2, List.of('C', 'D'), 0.4), "EF")));
+        System.out.println(prettyPrint(Stream.of("A", List.of(2, 3), "D").collect(Collectors.toMap(Object::hashCode, l -> l))));
+        System.out.println(prettyPrint(Stream.of("A", Set.of(2, 3), "D").collect(Collectors.toMap(Object::hashCode, l -> l))));
     }
 }
